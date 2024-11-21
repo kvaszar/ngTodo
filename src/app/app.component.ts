c666d178ba510de746419a1f752b7e38e7ba3e8f
kvaszar <157027054+kvaszar@users.noreply.github.com>
2024-11-21 15:04:20 +0300

    Add todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
+
+
+class TodoItem {
+    constructor(
+        public task: string,
+        public checked: boolean) {
+    }
+
+    change_state() {
+        this.checked = !this.checked;
+    }
+}
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, FormsModule, NgClass],
+    templateUrl: './app.component.html',
+    standalone: true,
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todolist';
+    list: TodoItem[] = [];
+    taskToAdd: string = "";
+
+    add_item(task: string) {
+        this.list.push(new TodoItem(task, false));
+    }
+
+    remove_item(item: TodoItem) {
+        this.list = this.list.filter(x => x !== item);
+    }
+
+    remove_completed() {
+        this.list = this.list.filter(x => !x.checked);
+    }
 }
